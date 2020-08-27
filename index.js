bd75125a1bed7b7fec29bcf9047e59a022e65c37
cherish-an1 <eanozie98@gmail.com>
2020-08-27 14:19:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function w3_open() {
+    document.getElementById("mysidebar").style.display="block";
+    document.getElementById("mySidebar").style.height ="250px";
+}
+function w3_close() {
+    document.getElementById("mysidebar").style.display="none";
+}
+
+var slideIndex = 0;
+ showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000);
+}
